db11dbf27b98265bf6016d702f2b1fad44f02078
Ammar Mohamad <ammarqazlombard@gmail.com>
2026-04-19 01:19:53 +0200

    Update app.py

--- phishing_URL_detector/app.py
+++ phishing_URL_detector/app.py
@@ -13,7 +13,7 @@ df.drop('index',axis=1,inplace=True)
 X=df.drop('Result',axis=1)
 y=df['Result']
 X_train ,X_test ,y_train ,y_test =train_test_split(X ,y ,test_size=0.2 ,stratify=y ,random_state=42)
-y_pred=predict(X_test)
+y_pred=model.predict(X_test)
 st.title("Phishing Website Detector")
 st.markdown("#### By supervision of Dr. Mahmoud Yasin")
 
